930dc987279f991f62b8a7f70667a98546abb7ae
Francisco Javier Ruano Vázquez <71827176+fruavaz636@users.noreply.github.com>
2022-02-08 14:31:24 +0100

    Add files via upload

--- jsSelector/js/funciones.js
+++ jsSelector/js/funciones.js
@@ -16,12 +16,23 @@ function iniciar() {
         alert(radios[i].value);        
     }*/
 
-    document.getElementsByTagName("p")[0].innerHTML = "Primer P";
-    document.getElementsByTagName("p")[0].onclick = mostrarMensaje;
+    //document.getElementsByTagName("p")[0].innerHTML = "Primer P";
+    //document.getElementsByTagName("p")[0].onclick = mostrarMensaje;
 
-    document.getElementsByClassName("clase")[1].onclick = mostrarTexto("Pulsado");
+    //document.getElementsByClassName("clase")[1].onclick = mostrarTexto("Pulsado");
+
+    //document.querySelector("#principal p:first-child").onclick = alerta;
+
+    var coleccion = document.querySelectorAll("#principal p");
+    var coleccion = document.getElementById("principal").querySelectorAll("p");
+    for (let i = 0; i < coleccion.length; i++) {
+        coleccion[i].innerHTML = "P " + (i);        
+    }
 }
 
+function alerta() {
+    alert("ALERTA");
+}
 
 
 function mostrarMensaje() {
