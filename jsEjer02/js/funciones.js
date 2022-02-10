69e65db54aa85edcf241962a3933ab3bb40b18ff
Francisco Javier Ruano Vázquez <71827176+fruavaz636@users.noreply.github.com>
2022-02-10 14:00:39 +0100

    Add files via upload

--- jsEjer02/js/funciones.js
+++ jsEjer02/js/funciones.js
@@ -2,9 +2,9 @@ function iniciar() {
     var titulo = document.getElementById("titulo");
     var texto = document.getElementById("texto");
 
-    var capa = document.getElementById("capa");
-    if (capa.classList.contains("success")) {
-        titulo.innerHTML = "CORRECTO";
+    var capa = document.getElementById("capa");    
+    if (document.getElementById("capa").className == "success") {
+        document.getElementById("titulo").innerHTML = "CORRECTO";
         texto.innerHTML = "Los datos son correctos";
     } else if (capa.classList.contains("error")) {
         titulo.innerHTML = "ERROR";
